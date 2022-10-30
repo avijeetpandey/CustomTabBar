45615bd22ff2eb87cf95887c892e594a6898fd02
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-10-30 14:40:19 +0530

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Avijeet Pandey on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
